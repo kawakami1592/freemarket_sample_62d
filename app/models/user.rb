5dd60f3b957cddb932d1cfd625843630e7f20f9b
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-02-07 20:56:51 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :lastname,:firstname,:lastname_kana,:firstname_kana, 
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
-
