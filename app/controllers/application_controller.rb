f48ff2b7d022089dbb18539eea12799eb2b545ae
KONTA2019 <knt_tmy@icloud.com>
2020-02-08 22:03:29 +0900

    Update application_controller.rb
    basic認証の準備として更新します

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  
+  #下記はサーバーサイド実装の記述
+  #before_action :authenticate_user!
+  # ログインしていないとルートに飛ぶ
+  #before_action :configure_permitted_parameters, if: :devise_controller?
+  # 登録時のストロングパラメータ追加
+  #protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname,:firstname,:zipcode,:pref,:city,:address,:buildingname,:phone,:birthyear,:birthmonth,:birthday,:lastname_kana,:firstname_kana])
+  # end
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
