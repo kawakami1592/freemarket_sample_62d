a1a8dca683fe6749a7078072b3f19aaf55cc5267
345dvl <k.f.kntn@gmail.com>
2020-02-24 17:23:25 +0900

    update JS a little

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,11 +1,8 @@
 $(function() {
-  $('#category').hover(
+  $('.header__nav__left__list').hover(
     function() {
-      //カーソルが重なった時
-      console.log("aa");
       $('.header__nav__left__list__categories__category').addClass('open');
     }, function() {
-      //カーソルが離れた時
       $('.header__nav__left__list__categories__category').removeClass('open');
     }
   );
