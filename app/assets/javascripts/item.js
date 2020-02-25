6a49ac04e734902aeefc8e3a689407c364f2bf38
345dvl <k.f.kntn@gmail.com>
2020-02-26 02:48:20 +0900

    pull-req response

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,9 +1,9 @@
 $(function() {
-  $('.header__nav__left__list').hover(
+  $('.top-header__nav__left__list').hover(
     function() {
-      $('.header__nav__left__list__categories__category').addClass('open');
+      $('.top-header__nav__left__list__categories__category').addClass('open');
     }, function() {
-      $('.header__nav__left__list__categories__category').removeClass('open');
+      $('.top-header__nav__left__list__categories__category').removeClass('open');
     }
   );
 });
