955107784f84b9cc25a1c20080937db56c493887
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-02-27 21:29:35 +0900

    見やすく修正

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,16 +1,23 @@
 class GuidesController < ApplicationController
+  
   def delivery
   end
+
   def price
   end
+  
   def prohibited_item
   end
+  
   def prohibited_conduct
   end
+  
   def counterfeit_goods
   end
+  
   def stolen_goods
   end
+  
   def seller_terms
   end
   
