587e95eff009cb2c8f72f262efe885127ce696f0
chois1003 <choe2ss@gmail.com>
2020-02-28 19:03:07 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # 下記はサーバーサイド実装の記述
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   # ログインしていないとルートに飛ぶ
   before_action :configure_permitted_parameters, if: :devise_controller?
   # 登録時のストロングパラメータ追加
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname,:firstname,:zipcode,:pref,:city,:address,:buildingname,:phone,:birthyear,:birthmonth,:birthday,:lastname_kana,:firstname_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname,:firstname,:zipcode,:pref_id,:city,:address,:buildingname,:phone,:birthyear_id,:birthmonth_id,:birthday_id,:lastname_kana,:firstname_kana])
   end
 
   private
