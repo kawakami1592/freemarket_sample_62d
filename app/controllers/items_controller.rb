3f8651e061386ff0b0339e65b526225f7f5ad04b
345dvl <k.f.kntn@gmail.com>
2020-03-07 10:49:22 +0900

    items#indexに商品情報取得のコードを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
   def index
-    # 仮メインページ
+    @items = Item.includes(:user).last(3)
   end
+
   def new
     @item = Item.new
   end
