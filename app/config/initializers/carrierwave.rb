af19cd6b3a6ace992c6776f32c1aba8f5dea13fc
KONTA2019 <knt_tmy@icloud.com>
2020-03-09 02:28:30 +0900

    Update carrierwave.rb

--- app/config/initializers/carrierwave.rb
+++ app/config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'freemarket-sample-62d'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-62d'
+    config.fog_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-62d'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
