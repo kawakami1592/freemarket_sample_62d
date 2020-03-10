79fc44c1f2b4228226b0d9ed1227069f0baae1ba
KONTA2019 <knt_tmy@icloud.com>
2020-03-11 06:52:00 +0900

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
 # ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', File.dirname(__FILE__))
+# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', File.dirname(__FILE__))
+
+root = "/var/www/freemarket_sample_62d/current"
+before_exec do |server|
+ENV['BUNDLE_GEMFILE'] = "#{root}/Gemfile"
+end
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
