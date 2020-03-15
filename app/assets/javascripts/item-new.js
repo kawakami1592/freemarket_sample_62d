9ecfa606beea5b63970b29b4f8993db03daea7ac
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-03-15 21:58:34 +0900

    手数料計算機能、画像削除後の挙動修正
    販売価格を入力すると手数料と利益を自動計算する
    画像の削除後、条件分岐を設置し9枚になった時に再度inputが生成されるようにした

--- app/assets/javascripts/item-new.js
+++ app/assets/javascripts/item-new.js
@@ -1,12 +1,13 @@
 $(document).on('turbolinks:load', function(){
   
   // 画像が選択された時プレビュー表示、inputの親要素のulをイベント元に指定
-  $('#output-box').on('change', function(e){
+  $('#image-input').on('change', function(e){
 
     //ファイルオブジェクトを取得する
     let files = e.target.files;
     $.each(files, function(index, file) {
       let reader = new FileReader();
+
       //画像でない場合は処理終了
       if(file.type.indexOf("image") < 0){
         alert("画像ファイルを指定してください。");
@@ -15,17 +16,12 @@ $(document).on('turbolinks:load', function(){
       //アップロードした画像を設定する
       reader.onload = (function(file){
         return function(e){
-          let itemLength = $('#output-box').children('li').length;
-          // 選択されたファイルのプレビューの数を数える
-
+          let imageLength = $('#output-box').children('li').length;
+          // 表示されているプレビューの数を数える
+          
           let labelLength = $("#image-input>label").eq(-1).data('label-id');
-          // ul要素の子要素labelの中から最後の要素のカスタムデータidを取得
+          // #image-inputの子要素labelの中から最後の要素のカスタムデータidを取得
 
-          if (itemLength == 9) {
-            $('[for=item_images9]').css('display','none');
-            //10枚選択されたら終わり
-            return false;
-          } else {
           // プレビュー表示
           $('#image-input').before(`<li class="upload-image${labelLength} data-image-id="${labelLength}">
                                       <figure class="upload-image__figure">
@@ -36,45 +32,71 @@ $(document).on('turbolinks:load', function(){
                                         <a class="upload-image__button__delete" data-image-id="${labelLength}">削除</a>
                                       </div>
                                     </li>`);
-
-          // 入力されたlabelを見えなくする or 表示されているlabelを見えなくする
           $("#image-input>label").eq(-1).css('display','none');
-         
-          // 新たにinputを生成する
-          $("#image-input").append(`<label for="item_images${labelLength+1}" class="sell-container__content__upload__items__box__label" data-label-id="${labelLength+1}">
-                                    <input multiple="multiple" class="sell-container__content__upload__items__box__input" id="item_images${labelLength+1}" style="display: none;" type="file" name="item[images][]">
-                                    <class="sell-container__content__upload__items__box__input__drop-box">
-                                      <i class="fas fa-camera"></i>
-                                      <pre class="sell-container__content__upload__items__box__input__drop-box__text">ドラッグアンドドロップ
-                                      またはクリックしてファイルをアップロード</pre>
-                                    </pre>
-                                  </label>`);
-        }}
+          // 入力されたlabelを見えなくする
+
+          if (imageLength < 9) {
+            // 表示されているプレビューが９以下なら、新たにinputを生成する
+            $("#image-input").append(`<label for="item_images${labelLength+1}" class="sell-container__content__upload__items__box__label" data-label-id="${labelLength+1}">
+                                        <input multiple="multiple" class="sell-container__content__upload__items__box__input" id="item_images${labelLength+1}" style="display: none;" type="file" name="item[images][]">
+                                        <class="sell-container__content__upload__items__box__input__drop-box">
+                                          <i class="fas fa-camera"></i>
+                                          <pre class="sell-container__content__upload__items__box__input__drop-box__text">ドラッグアンドドロップ
+                                          またはクリックしてファイルをアップロード</pre>
+                                        </pre>
+                                      </label>`);
+          };
+        };
       })(file);
       reader.readAsDataURL(file);
     });
-    
   });
 
   //削除ボタンが押された時
   $(document).on('click', '.upload-image__button__delete', function(){
     let targetImageId = $(this).data('image-id');
     // イベント元のカスタムデータ属性の値を取得
-    
     $(`.upload-image${targetImageId}`).remove();
     //プレビューを削除
-
     $(`[for=item_images${targetImageId}]`).remove();
     //削除したプレビューに関連したinputを削除
 
+    let imageLength = $('#output-box').children('li').length;
+    // 表示されているプレビューの数を数える
+    if (imageLength ==9) {
+      let labelLength = $("#image-input>label").eq(-1).data('label-id');
+      // 表示されているプレビューが９なら,#image-inputの子要素labelの中から最後の要素のカスタムデータidを取得
+      $("#image-input").append(`<label for="item_images${labelLength+1}" class="sell-container__content__upload__items__box__label" data-label-id="${labelLength+1}">
+                                  <input multiple="multiple" class="sell-container__content__upload__items__box__input" id="item_images${labelLength+1}" style="display: none;" type="file" name="item[images][]">
+                                  <class="sell-container__content__upload__items__box__input__drop-box">
+                                    <i class="fas fa-camera"></i>
+                                    <pre class="sell-container__content__upload__items__box__input__drop-box__text">ドラッグアンドドロップ
+                                    またはクリックしてファイルをアップロード</pre>
+                                  </pre>
+                                </label>`);
+    };
   });
 
   // f.text_areaの文字数カウント
-  $(function () {
-    $("textarea").keyup(function(){
-      let txtcount = $(this).val().length;
-      $("#word-count").text(txtcount);
-    })
+  $("textarea").keyup(function(){
+    let txtcount = $(this).val().length;
+    $("#word-count").text(txtcount);
+  });
+
+  //販売価格入力時の手数料計算
+  $('#item_price').keyup(function(){
+    let price= $(this).val();
+    if (price >= 300 && price <= 9999999){
+      let fee = Math.floor(price * 0.1);
+      // 小数点以下切り捨て
+      let profit = (price - fee);
+      $('.sell-container__content__commission__right').text('¥'+fee.toLocaleString());
+      // 対象要素の文字列書き換える
+      $('.sell-container__content__profit__right').text('¥'+profit.toLocaleString());
+    } else{
+      $('.sell-container__content__commission__right').html('ー');
+      $('.sell-container__content__profit__right').html('ー');
+    }
   });
 
 });
