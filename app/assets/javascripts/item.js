2571ccdaba9c5f8c8f34d3a7825d3aa04b40e30f
345dvl <k.f.kntn@gmail.com>
2020-03-21 14:53:04 +0900

    コメントアウト文を削除

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,13 +1,3 @@
-// $(function() {
-//   $('.top-header__nav__left__list').hover(
-//     function() {
-//       $('.top-header__nav__left__list__categories__category').addClass('open');
-//     }, function() {
-//       $('.top-header__nav__left__list__categories__category').removeClass('open');
-//     }
-//   );
-// });
-
 $(document).on('turbolinks:load', function() {
   $(function(){
     $('.top-header__nav__left__list').on({
