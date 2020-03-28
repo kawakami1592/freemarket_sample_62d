0cd54364ef7640d14fb3514cc8f5509857c4c524
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-03-28 21:29:42 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
   belongs_to_active_hash :birthmonth
   belongs_to_active_hash :birthday
 
+  validates :nickname, :encrypted_password, :lastname, :firstname, :zipcode, :pref_id, :city, :address, :lastname_kana, :firstname_kana, :birthyear_id, :birthmonth_id, :birthday_id, presence: true
+  validates :password, presence: true, length: { minimum: 7 }, confirmation: true
+  validates :password_confirmation, presence: true, length: { minimum: 7 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :lastname,:firstname,:lastname_kana,:firstname_kana, 
-  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   devise :database_authenticatable, :registerable
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module FreemarketSample62d
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    # config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
     # 日本語化
   end
 end
