a0c9658a1fe7963c70fae4f6bba6c9707b40b081
numazaki345 <ryo.091979@gmail.com>
2020-04-10 22:50:57 +0900

    修正

--- app/assets/javascripts/datailpage.js
+++ app/assets/javascripts/datailpage.js
@@ -1,22 +1,14 @@
-$(document).on('turbolinks:load',function() {
-  $('img.thumb').click(function(){
-    // "_thumb"を削った画像ファイル名を取得
-
-  var selectedSrc = $(this).attr('src').replace(/^(.+)_thumb(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
-
-  // 画像入れ替え
-  $('img.mainImage').stop().fadeOut(50,
-    function(){
-      $('img.mainImage').attr('src', selectedSrc);
-      $('img.mainImage').stop().fadeIn(200);
-    }
-  );
-  // サムネイルの枠を変更
-  $(this).css({"border":"2px solid #ff5a71"});
-  });
-
-  // マウスアウトでサムネイル枠もとに戻す
-  $('img.thumb').mouseout(function(){
-  $(this).css({"border":""});
-  });
+$(document).on('turbolinks:load',function(){
+    $('img.thumb').click(function(){
+        var selectedSrc = $(this).attr('src').replace(/^(.+)_thumb(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
+        $('img.mainImage').stop().fadeOut(50,
+            function(){
+                $('img.mainImage').attr('src',selectedSrc);
+                $('img.mainImage').stop().fadeIn(200);
+            });
+        $(this).css({"border":"2px solid #ff5a71"});
+    });
+    $('img.thumb').mouseout(function(){
+        $(this).css({"border":""});
+    });
 });
